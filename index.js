d52751190de6b18878946edc91f5c777cecf3012
grantbergland <grantbergland@gmail.com>
2022-07-06 14:59:44 -0500

    commit tree 48f7d33e4bd023721b6bd5ee22071a8fe1b77a70

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+const employee = {
+    //obj=employee
+    //key=name
+    //value=sam
+};
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const employeeCopy = { ...obj };
+    employeeCopy[key] = value;
+    return employeeCopy;
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj;
+};
+function deleteFromEmployeeByKey(employee, key, value){
+    const deleteEmployee = {...employee};
+    deleteEmployee[key] = value
+    return deleteEmployee;
+}    
+function destructivelyDeleteFromEmployeeByKey(obj, key, value){
+    obj[key] = value
+    return employee
+}
